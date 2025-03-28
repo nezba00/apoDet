a66802e9a4f1bef855762cd0a4df0c52340f596d
nezba00 <nbahou98@gmail.com>
2025-03-28 16:34:11 +0100

    Update __init__ so it contains config

--- scripts/preprocessing/__init__.py
+++ scripts/preprocessing/__init__.py
@@ -3,11 +3,15 @@ from .segmentation import run_segmentation, filter_segmentation
 from .tracking import run_tracking, convert_obj_to_track_ids
 from .matching import match_annotations, check_temporal_compatibility
 from .cropping import crop_window
+from .config import (SEGMENTATION_CONFIG, TRACKING_CONFIG,
+                     APO_MATCH_CONFIG, APO_CROP_CONFIG)
 
 __all__ = [
     "load_image_stack", "get_image_paths",
     "run_segmentation", "filter_segmentation",
     "run_tracking", "convert_obj_to_track_ids",
     "match_annotations", "check_temporal_compatibility",
-    "crop_window"
+    "crop_window",
+    "SEGMENTATION_CONFIG", "TRACKING_CONFIG",
+    "APO_MATCH_CONFIG", "APO_CROP_CONFIG"
 ]
